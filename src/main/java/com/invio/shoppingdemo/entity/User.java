809d3e49b5a3260484c030c422167af5057d006d
Murat Simsek <muratsimseekk@gmail.com>
2024-04-26 14:53:53 +0300

    Creates User entity

---
+++ src/main/java/com/invio/shoppingdemo/entity/User.java
@@ -0,0 +1,31 @@
+package com.invio.shoppingdemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user" , schema = "shopping")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
